a7215cf84e60450793b0de151424f700272840a4
itsFM-Codes <hanskartawinata@gmail.com>
2026-07-17 11:53:13 +0700

    Feat: Implemented quiescence search

--- src/engine/search.cpp
+++ src/engine/search.cpp
@@ -70,6 +70,80 @@ static void order_moves(const Position *position, MoveList *moves) {
     }
 }
 
+static int position_is_in_check(const Position *position) {
+    int king_square = find_king(position, position->side_to_move);
+
+    return is_valid_square(king_square) &&
+           is_square_attacked(
+               position,
+               king_square,
+               opposite_color(position->side_to_move)
+           );
+}
+
+static int quiescence(
+    Position *position,
+    int alpha,
+    int beta,
+    int ply
+) {
+    MoveList moves;
+    int in_check;
+    int index;
+
+    generate_legal_moves(position, &moves);
+    in_check = position_is_in_check(position);
+
+    if (moves.count == 0) {
+        if (in_check) {
+            return -SEARCH_CHECKMATE + ply;
+        }
+
+        return 0;
+    }
+
+    if (!in_check) {
+        int stand_pat = evaluate_position(position);
+
+        if (stand_pat >= beta) {
+            return beta;
+        }
+
+        if (stand_pat > alpha) {
+            alpha = stand_pat;
+        }
+    }
+
+    order_moves(position, &moves);
+
+    for (index = 0; index < moves.count; ++index) {
+        Move move = moves.moves[index];
+        UndoState undo;
+        int score;
+
+        if (!in_check && (move.flags & MOVE_FLAG_CAPTURE) == 0) {
+            continue;
+        }
+
+        if (!make_move(position, move, &undo)) {
+            continue;
+        }
+
+        score = -quiescence(position, -beta, -alpha, ply + 1);
+        undo_move(position, move, &undo);
+
+        if (score >= beta) {
+            return beta;
+        }
+
+        if (score > alpha) {
+            alpha = score;
+        }
+    }
+
+    return alpha;
+}
+
 static int negamax(
     Position *position,
     int depth,
@@ -81,7 +155,7 @@ static int negamax(
     int index;
 
     if (depth <= 0) {
-        return evaluate_position(position);
+        return quiescence(position, alpha, beta, ply);
     }
 
     generate_legal_moves(position, &moves);
--- tests/search_test.cpp
+++ tests/search_test.cpp
@@ -66,10 +66,36 @@ static void test_iterative_search_reaches_requested_depth(void) {
     assert(best_move.to == make_square(0, 3));
 }
 
+static void test_quiescence_sees_a_recapture(void) {
+    Position position;
+    Move best_move;
+
+    clear_position(&position);
+    position_set_piece(&position, make_square(7, 0), PIECE_WHITE_KING);
+    position_set_piece(&position, make_square(7, 3), PIECE_WHITE_QUEEN);
+    position_set_piece(&position, make_square(0, 3), PIECE_BLACK_ROOK);
+    position_set_piece(&position, make_square(0, 4), PIECE_BLACK_QUEEN);
+    position_set_piece(&position, make_square(0, 7), PIECE_BLACK_KING);
+    position.side_to_move = COLOR_WHITE;
+
+    assert(search_position(&position, 1, &best_move) == -ROOK_VALUE);
+    assert(!(best_move.from == make_square(7, 3) &&
+             best_move.to == make_square(0, 3)));
+}
+
+static void test_quiescence_keeps_starting_position_equal(void) {
+    Position position;
+
+    set_starting_position(&position);
+    assert(search_position(&position, 5, 0) == 0);
+}
+
 int main(void) {
     test_zero_depth_evaluates_position();
     test_search_captures_hanging_rook();
     test_search_detects_checkmate();
     test_iterative_search_reaches_requested_depth();
+    test_quiescence_sees_a_recapture();
+    test_quiescence_keeps_starting_position_equal();
     return 0;
 }
