ab0c8f3d34829f32c4fb6795e57aabd46349af83
itsFM-Codes <hanskartawinata@gmail.com>
2026-07-17 11:40:36 +0700

    Feat: Search engine now checks for captures first (with MVV-LVA)

--- src/engine/search.cpp
+++ src/engine/search.cpp
@@ -3,6 +3,73 @@
 #include "src/chess/movegen.h"
 #include "src/eval/evaluation.h"
 
+static int piece_value(Piece piece) {
+    switch (piece_type(piece)) {
+        case PIECE_TYPE_PAWN:
+            return PAWN_VALUE;
+        case PIECE_TYPE_KNIGHT:
+            return KNIGHT_VALUE;
+        case PIECE_TYPE_BISHOP:
+            return BISHOP_VALUE;
+        case PIECE_TYPE_ROOK:
+            return ROOK_VALUE;
+        case PIECE_TYPE_QUEEN:
+            return QUEEN_VALUE;
+        case PIECE_TYPE_KING:
+            return KING_VALUE;
+        case PIECE_TYPE_NONE:
+            return 0;
+    }
+
+    return 0;
+}
+
+static int move_order_score(const Position *position, Move move) {
+    Piece attacker;
+    Piece victim;
+
+    if ((move.flags & MOVE_FLAG_CAPTURE) == 0) {
+        return 0;
+    }
+
+    attacker = position_piece_at(position, move.from);
+
+    if (move.flags & MOVE_FLAG_EN_PASSANT) {
+        victim = PIECE_WHITE_PAWN;
+    } else {
+        victim = position_piece_at(position, move.to);
+    }
+
+    return 10000 + piece_value(victim) * 16 - piece_value(attacker);
+}
+
+static void order_moves(const Position *position, MoveList *moves) {
+    int index;
+
+    if (position == 0 || moves == 0) {
+        return;
+    }
+
+    for (index = 0; index < moves->count; ++index) {
+        int best_index = index;
+        int next;
+
+        for (next = index + 1; next < moves->count; ++next) {
+            if (move_order_score(position, moves->moves[next]) >
+                move_order_score(position, moves->moves[best_index])) {
+                best_index = next;
+            }
+        }
+
+        if (best_index != index) {
+            Move move = moves->moves[index];
+
+            moves->moves[index] = moves->moves[best_index];
+            moves->moves[best_index] = move;
+        }
+    }
+}
+
 static int negamax(
     Position *position,
     int depth,
@@ -18,6 +85,7 @@ static int negamax(
     }
 
     generate_legal_moves(position, &moves);
+    order_moves(position, &moves);
 
     if (moves.count == 0) {
         int king_square = find_king(position, position->side_to_move);
@@ -80,6 +148,7 @@ int search_position(Position *position, int depth, Move *best_move) {
     }
 
     generate_legal_moves(position, &moves);
+    order_moves(position, &moves);
 
     if (moves.count == 0) {
         int king_square = find_king(position, position->side_to_move);
